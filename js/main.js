fd05e0f062845407d3d90479d3151e15c82ea323
Dilmurod03 <dahmadjonov1031@gmail.com>
2022-07-02 21:31:20 +0500

    style finished

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
-  const API = `https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}` 
-  const searchBox = document.querySelector('.search-box');
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".input");
+let elList = document.querySelector(".wrapper-left");
 
-searchBox.addEventListener("keypress", setQuery);
+let renderCountrys = function (data) {
+  let html = `
+  <h2 class="wrapper-head">${data.name}</h2>
+  <div class="wrapper-country">Country:${data.sys.country}</div>
+  <div class="wrapper-Temper">${data.main.temp}</div>
+  <div class="wrapper-speed">Speed:${data.wind.speed}</div>
+  `;
 
-function setQuery(evt) {
-  if(evt.keyCode - 13) {
-    console.log(searchBox.value);
+  elList.innerHTML = null;
+  elList.insertAdjacentHTML("beforeend", html);
+};
+
+let main = async function (country) {
+  try {
+    let Fetch = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=277e160f5af509c9f6e384d7cbe3501c`
+    );
+    let newFetch = await Fetch.json();
+    console.log(newFetch);
+
+    renderCountrys(newFetch);
+  } catch (error) {
+    console.log(error.message);
   }
-}
+};
 
-function getResults(query) {
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-fetch(`${API}weather?q=${query}&units=metric&APPID=${API}`)
-  .then(weather => { 
-    return weather.json();
-  }).then(displayResults);
-}
+  let InputValue = elInput.value;
+  elInput.innerHTML = null;
+  elInput.value = null;
 
-function displayResults(weather) {
-  console.log(weather);
-}
+  main(InputValue);
+});
+
+main("Tashkent");
